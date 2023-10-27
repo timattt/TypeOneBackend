e72fca81000c61ecff454088f36742c963c12f80
timattt <timattt@gmail.com>
2023-10-27 19:28:17 +0300

    initial commit

---
+++ src/main/java/org/shlimtech/TypeOneBackend/TypeOneBackendApplication.java
@@ -0,0 +1,13 @@
+package org.shlimtech.TypeOneBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypeOneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypeOneBackendApplication.class, args);
+	}
+
+}
